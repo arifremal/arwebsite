4821e7b64205d9f776e9e36fea176a88ec9594bd
Ariful Islam <remalarif0130@gmail.com>
2024-07-07 02:59:18 +0600

    aaded

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,16 @@ import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
 
-
 // parser
 app.use(express.json())
-app.use(cors ())
+app.use(cors())
 
-app.get('/', (req:Request, res:Response) => {
-    const a= 10;
-  res.send(a)
+app.get('/', (req: Request, res: Response) => {
+  
+  res.send()
 })
 
 console.log(process.cwd())
 
 export default app;
+
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,36 @@
-import mongoose from "mongoose";
+// import mongoose from 'mongoose';
+// import app from './app';
+// import config from './app/config';
+
+// async function main() {
+//   try {
+//     await mongoose.connect(config.databas_url as string);
+
+//     app.listen(config.port, () => {
+//       console.log(`app is listening on port ${config.port}`);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// main();
 import config from "./app/config";
 import app from "./app";
 
+// const mongoose = require('mongoose');
+import mongoose from "mongoose";
+
 async function main() {
-  
-  try{
-    await mongoose.connect(config.databas_url as string);
+  try {
+    await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`app listening on port ${config.port}`);
     });
-    
+  } catch (err) {
+    console.log(err);
   }
-  catch(err){
-    console.log(err)
-  }
-
-
 }
-main()
+
+main();
